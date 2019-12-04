55f9388e96e4483a5bc1435847e94d098071626c
Matthew Yokopovich <matthew.yokopovich@gmail.com>
2019-12-03 18:02:23 -0600

    testing

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,9 +2,11 @@ const db = require("../models");
 
 module.exports = {
     findUser: function(req, res){
+        console.log("test3");
         db.User.find(req.steamId).then(dbUser=> res.json(dbUser));
     },
     createUser: function(req, res){
+        console.log("test3");
         db.User.create(req.body).then(dbUser => res.json(dbUser));
     },
     updateUser: function(req, res){
