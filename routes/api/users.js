895d64d4b5908e1e49de427bfe5e932de20a7ce7
Matthew Yokopovich <matthew.yokopovich@gmail.com>
2019-12-03 18:57:39 -0600

    D:

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,13 +2,14 @@ const router = require("express").Router();
 const db = require("../../models");
 
 router.route("/find").post((req, res)=>{
+    console.log(req.body);
     db.User.find({
         steamId: req.body.steamId
     }).then(dbUser=> res.json(dbUser));
 });
 router.route("/create").put((req, res)=>{
     console.log(req.body);
-    db.User.create(req.body).then(dbUser => res.json(dbUser));
+    db.User.create(req.body.user).then(dbUser => res.json(dbUser));
 });
 router.route("/update").post((req, res)=>{
     db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
