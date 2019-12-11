307a2354e3953374b1346d2fd42de03a09c50d88
Matthew Yokopovich <matthew.yokopovich@gmail.com>
2019-12-11 12:21:47 -0600

    D:

--- client/src/pages/SearchResults.js
+++ client/src/pages/SearchResults.js
@@ -77,6 +77,7 @@ class SearchResults extends Component {
                                 loggedIn
                             })
                             console.log(this.state);  
+                            console.log(commentResp.data);
                                             })
                                         
                                     })
