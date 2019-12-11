399d8fcd0e61d22dcff7041b59058756a3bd485e
Matthew Yokopovich <matthew.yokopovich@gmail.com>
2019-12-11 13:02:42 -0600

    D:

--- client/src/components/Comment.js
+++ client/src/components/Comment.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 class Comment extends Component{
 
+    componentDidMount(){
+        console.log(this.props.author);
+    }
+
     render(){
         return (
             <div>
